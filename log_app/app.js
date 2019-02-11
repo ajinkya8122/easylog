0f78127ccd6e6f4e84e92c714be311bcaf119736
Ajinkya Bedekar <ajinkya8122@gmail.com>
2019-02-11 06:01:06 +0000

    add mysql and set app config

--- log_app/app.js
+++ log_app/app.js
@@ -1,7 +1,32 @@
-var express = require(express),
-    app     = express();
+var express     = require("express"),
+    mysql       = require("mysql"),
+    bodyParser  = require('body-parser'),
+    app         = express();
     
+    
+// APP CONFIG
+app.set('view-engine', 'ejs');
 
+//user ajinkya8122
+var connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'ajinkya8122',
+   database: 'log_app'
+});
+
+var q = 'SELECT * FROM users';
+
+connection.query(q, function(err, results, fields){
+    if(err) throw err;
+    console.log(results);
+});
+
+
+//ROUTES
 app.get("/", function(req, res){
     res.send("this is home!"); 
+});
+
+app.listen(process.env.PORT, function(){
+    console.log("App is running!")
 });
